de37737ff76bcdf2e51833dbf7bcfe0a74c69b10
imantung <iman.tung@gmail.com>
2019-02-05 10:11:38 +0700

    add reference in comment

--- app/server/middlewares.go
+++ app/server/middlewares.go
@@ -5,4 +5,8 @@ import "github.com/labstack/echo/middleware"
 func initMiddlewares(server *Server) {
 	server.Use(middleware.Logger())
 	server.Use(middleware.Recover())
+	// check list of middleware at https://echo.labstack.com/middleware
 }
+
+// Put custom middleware belows
+// Example: https://echo.labstack.com/cookbook/middleware
