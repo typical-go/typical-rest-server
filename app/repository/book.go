747b37e29a78c20dc4adb0a6617c9f041ad847ce
imantung <iman.tung@gmail.com>
2019-02-28 08:24:17 +0700

    add validation for book model

--- app/repository/book.go
+++ app/repository/book.go
@@ -3,6 +3,8 @@ package repository
 import (
 	"database/sql"
 	"time"
+
+	validator "gopkg.in/go-playground/validator.v9"
 )
 
 var (
@@ -13,12 +15,18 @@ var (
 // Book represented database model
 type Book struct {
 	ID        int       `json:"id"`
-	Title     string    `json:"title"`
-	Author    string    `json:"author"`
+	Title     string    `json:"title" validate:"required"`
+	Author    string    `json:"author" validate:"required"`
 	CreatedAt time.Time `json:"created_at"`
 }
 
 func scanBook(rows *sql.Rows) (book Book, err error) {
 	err = rows.Scan(&book.ID, &book.Title, &book.Author, &book.CreatedAt)
 	return
+}
+
+// Validate book
+func (b *Book) Validate() error {
+	validate := validator.New()
+	return validate.Struct(b)
 }
