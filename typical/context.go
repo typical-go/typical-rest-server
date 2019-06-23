1e0360812d6a97fe5811b0e6d5fe43706a51cee1
iman tung <iman.tung@gmail.com>
2019-06-23 19:01:47 +0700

    add config prefix

--- typical/context.go
+++ typical/context.go
@@ -5,7 +5,8 @@ import "github.com/typical-go/typical-go/appx"
 // Context instance of Context
 // TODO: consider to use yaml file
 var Context = appx.Context{
-	Name:        "Typical-RESTful-Server",
-	Version:     "0.1.0",
-	Description: "Example of typical and scalable RESTful API Server for Go",
+	Name:         "Typical-RESTful-Server",
+	Version:      "0.1.0",
+	ConfigPrefix: "APP",
+	Description:  "Example of typical and scalable RESTful API Server for Go",
 }
