2e3c6cadf70c505bbf7973d99e2dcc121a90761b
iman tung <iman.tung@gmail.com>
2019-06-29 22:08:07 +0700

    using urfave cli for application main

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,13 +1,27 @@
 package main
 
 import (
+	"os"
+
 	_ "github.com/lib/pq"
 	"github.com/typical-go/typical-rest-server/app"
 	"github.com/typical-go/typical-rest-server/typical"
+	"gopkg.in/urfave/cli.v1"
 )
 
 func main() {
-	typical.Context.Container().Invoke(func(s *app.Server) error {
+	app := cli.NewApp()
+	app.Name = typical.Context.Name
+	app.Usage = ""
+	app.Description = typical.Context.Description
+	app.Version = typical.Context.Version
+	app.Action = actionFunction
+
+	app.Run(os.Args)
+}
+
+func actionFunction(ctx *cli.Context) error {
+	return typical.Context.Container().Invoke(func(s *app.Server) error {
 		return s.Serve()
 	})
 }
