844da64735842c75af7eaa94ff087ac79f8e54ea
iman tung <iman.tung@gmail.com>
2019-08-09 12:16:46 +0700

    bump version 0.4.4

--- typical/context.go
+++ typical/context.go
@@ -12,7 +12,7 @@ import (
 // Context instance of Context
 var Context = typictx.Context{
 	Name:        "Typical-RESTful-Server",
-	Version:     "0.4.3",
+	Version:     "0.4.4",
 	Description: "Example of typical and scalable RESTful API Server for Go",
 
 	Configs: []typictx.Config{
