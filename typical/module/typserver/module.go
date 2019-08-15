b7c5678419e646f3fa519cf8dd8525da3892f6d7
iman tung <iman.tung@gmail.com>
2019-08-15 19:33:40 +0700

    idempotent for echo server + tidy up

--- typical/module/typserver/module.go
+++ typical/module/typserver/module.go
@@ -1,52 +1,16 @@
 package typserver
 
 import (
-	"context"
-	"time"
-
-	logrusmiddleware "github.com/bakatz/echo-logrusmiddleware"
-	"github.com/labstack/echo"
-	log "github.com/sirupsen/logrus"
 	"github.com/typical-go/typical-rest-server/EXPERIMENTAL/typictx"
 )
 
 // Module for postgres
-func Module(logHooks ...log.Hook) *typictx.Module {
+func Module() *typictx.Module {
 	return &typictx.Module{
 		Name:         "Echo Server with Logrus",
 		ConfigPrefix: "SERVER",
 		ConfigSpec:   &Config{},
-		OpenFunc: func(cfg *Config) (server *echo.Echo, err error) {
-			server = echo.New()
-			server.HideBanner = true
-			server.Debug = cfg.Debug
-
-			logrusMwConfig := logrusmiddleware.Config{}
-
-			// set log
-			if cfg.Debug {
-				log.SetLevel(log.InfoLevel)
-				logrusMwConfig.IncludeRequestBodies = true
-				logrusMwConfig.IncludeResponseBodies = true
-				log.Info("Configure logger for debug mode")
-			} else {
-				log.SetLevel(log.WarnLevel)
-				log.SetFormatter(&log.JSONFormatter{})
-			}
-
-			for _, hook := range logHooks {
-				log.AddHook(hook)
-			}
-
-			server.Use(logrusmiddleware.HookWithConfig(logrusMwConfig))
-			server.Logger = logrusmiddleware.Logger{Logger: log.StandardLogger()}
-			return
-		},
-		CloseFunc: func(server *echo.Echo) error {
-			log.Info("Server is shutting down")
-			ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-			defer cancel()
-			return server.Shutdown(ctx)
-		},
+		OpenFunc:     Create,
+		CloseFunc:    Shutdown,
 	}
 }
---
+++ typical/module/typserver/server.go
@@ -0,0 +1,47 @@
+package typserver
+
+import (
+	"context"
+	"time"
+
+	logrusmiddleware "github.com/bakatz/echo-logrusmiddleware"
+	"github.com/labstack/echo"
+	log "github.com/sirupsen/logrus"
+)
+
+var server *echo.Echo
+
+// Create new server
+func Create(cfg *Config) *echo.Echo {
+	if server == nil {
+		server = echo.New()
+		server.HideBanner = true
+		server.Debug = cfg.Debug
+
+		logrusMwConfig := logrusmiddleware.Config{}
+
+		// set log
+		if cfg.Debug {
+			log.SetLevel(log.InfoLevel)
+			logrusMwConfig.IncludeRequestBodies = true
+			logrusMwConfig.IncludeResponseBodies = true
+			log.Info("Configure logger for debug mode")
+		} else {
+			log.SetLevel(log.WarnLevel)
+			log.SetFormatter(&log.JSONFormatter{})
+		}
+
+		server.Use(logrusmiddleware.HookWithConfig(logrusMwConfig))
+		server.Logger = logrusmiddleware.Logger{Logger: log.StandardLogger()}
+	}
+
+	return server
+}
+
+// Shutdown the server
+func Shutdown(server *echo.Echo) error {
+	log.Info("Server is shutting down")
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	return server.Shutdown(ctx)
+}
