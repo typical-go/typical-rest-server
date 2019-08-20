37ee1966f9bee01216200b833316285554b9b255
iman tung <iman.tung@gmail.com>
2019-08-20 15:10:27 +0700

    return error if there is uncommitted change instead of info

--- EXPERIMENTAL/typitask/release_distribution.go
+++ EXPERIMENTAL/typitask/release_distribution.go
@@ -42,8 +42,7 @@ func ReleaseDistribution(action *typictx.ActionContext) (err error) {
 	worktree, _ := gitRepo.Worktree()
 	status, _ := worktree.Status()
 	if !status.IsClean() {
-		log.Infof("Please submit uncommitted change first:\n%s", status.String())
-		return nil
+		return fmt.Errorf("Please submit uncommitted change first:\n%s", status.String())
 	}
 
 	changelogs := listChangeLogs(gitRepo, latestTag)
