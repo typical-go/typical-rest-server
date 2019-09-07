6808fdf0e86df68649003592e8d33f6f088265a9
iman tung <iman.tung@gmail.com>
2019-09-07 14:02:58 +0700

    validate empty commit message to prevent error

--- EXPERIMENTAL/typirelease/typirelease.go
+++ EXPERIMENTAL/typirelease/typirelease.go
@@ -106,7 +106,7 @@ func GithubRelease(binaries, changeLogs []string, rel typictx.Release) (err erro
 	return
 }
 
-func cleanMessage(message string) string {
+func clean(message string) string {
 	iCoAuthor := strings.Index(message, "Co-Authored-By")
 	if iCoAuthor > 0 {
 		message = message[0:strings.Index(message, "Co-Authored-By")]
@@ -116,11 +116,18 @@ func cleanMessage(message string) string {
 }
 
 func ignoring(changelog string) bool {
-	message := cleanMessage(changelog[7:])
+	message := clean(message(changelog))
 	lowerMessage := strings.ToLower(message)
 	return strings.HasPrefix(lowerMessage, "merge") ||
 		strings.HasPrefix(lowerMessage, "bump") ||
 		strings.HasPrefix(lowerMessage, "revision") ||
 		strings.HasPrefix(lowerMessage, "generate") ||
 		strings.HasPrefix(lowerMessage, "wip")
+}
+
+func message(changelog string) string {
+	if len(changelog) < 7 {
+		return ""
+	}
+	return strings.TrimSpace(changelog[7:])
 }
