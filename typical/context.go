d33172ed053546af3378ff121dc9166d58d4cdd8
iman tung <iman.tung@gmail.com>
2019-10-11 18:15:49 +0700

    test versioning using dash symbol

--- typical/context.go
+++ typical/context.go
@@ -31,7 +31,7 @@ var Context = &typictx.Context{
 		typredis.Module(),
 	},
 	Release: typictx.Release{
-		Version: "0.8.1",
+		Version: "0.8.1-1",
 		Targets: []string{
 			"linux/amd64",
 			"darwin/amd64",
