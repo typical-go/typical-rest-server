df64147ff11f2d601f412a6b202b375122fa0dab
iman tung <iman.tung@gmail.com>
2019-11-03 22:22:46 +0700

    combine start, route and middleware in one file for convenience

---
+++ app/app.go
@@ -0,0 +1,33 @@
+package app
+
+import (
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+	"github.com/typical-go/typical-rest-server/app/config"
+	"github.com/typical-go/typical-rest-server/app/controller"
+	"go.uber.org/dig"
+)
+
+type params struct {
+	dig.In
+	*echo.Echo
+	*config.Config
+	controller.BookCntrl
+	controller.AppCntrl
+}
+
+// Routes of API
+func Routes(p params) {
+	p.AppCntrl.Route(p.Echo)
+	p.BookCntrl.Route(p.Echo)
+}
+
+// Middlewares for the service
+func Middlewares(p params) {
+	p.Echo.Use(middleware.Recover())
+}
+
+// Start the service
+func Start(p params) error {
+	return p.Echo.Start(p.Config.Address)
+}
