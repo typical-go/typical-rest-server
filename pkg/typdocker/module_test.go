c2bd26fd495400f12e9deb1c135e4becdc269009
iman tung <iman.tung@gmail.com>
2019-11-05 15:28:46 +0700

    revision: test on docker module to check is build CLI

--- pkg/typdocker/module_test.go
+++ pkg/typdocker/module_test.go
@@ -1,7 +1,14 @@
 package typdocker_test
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"github.com/typical-go/typical-rest-server/EXPERIMENTAL/typictx"
+	"github.com/typical-go/typical-rest-server/pkg/typdocker"
+)
 
 func TestModule(t *testing.T) {
-
+	m := typdocker.Module()
+	require.True(t, typictx.IsBuildCLI(m))
 }
