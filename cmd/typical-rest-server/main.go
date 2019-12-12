8d674855a690062dc1216db963bf9cb7cb13fac4
iman tung <iman.tung@gmail.com>
2019-12-12 18:11:24 +0700

    update to typical-go 0.9.12 + remove internal/dependency

--- cmd/typical-rest-server/main.go
+++ cmd/typical-rest-server/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"github.com/typical-go/typical-go/pkg/typapp"
-	_ "github.com/typical-go/typical-rest-server/internal/dependency"
 	"github.com/typical-go/typical-rest-server/typical"
 )
 
