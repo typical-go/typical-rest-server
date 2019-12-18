e91caa5a7df835e9d71c3c2e84a8bdbeab70076e
iman tung <iman.tung@gmail.com>
2019-12-18 11:22:56 +0700

    update to typical-go 0.9.18 + fix test

--- pkg/typrest/typrest_test.go
+++ pkg/typrest/typrest_test.go
@@ -5,13 +5,9 @@ import (
 
 	"github.com/stretchr/testify/require"
 	"github.com/typical-go/typical-go/pkg/typcore"
-	"github.com/typical-go/typical-go/pkg/typmodule"
 )
 
 func TestModule(t *testing.T) {
 	m := &Module{}
-	require.True(t, typmodule.IsProvider(m))
-	require.True(t, typmodule.IsDestroyer(m))
-	require.True(t, typmodule.IsProvider(m))
 	require.True(t, typcore.IsBuildCommander(m))
 }
