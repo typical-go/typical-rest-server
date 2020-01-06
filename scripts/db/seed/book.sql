b67e2bf65e8bf1f5e5ba7c37f538fcc3036d0fac
iman tung <iman.tung@gmail.com>
2020-01-06 22:11:43 +0700

    update autoincrement after db seed

--- scripts/db/seed/book.sql
+++ scripts/db/seed/book.sql
@@ -3,3 +3,5 @@ INSERT INTO books (id, title, author) VALUES (2, 'Leo Tolstoy', 'War and Peace')
 INSERT INTO books (id, title, author) VALUES (3, 'William Shakespeare', 'Hamlet');
 INSERT INTO books (id, title, author) VALUES (4, 'Homer', 'The Odyssey');
 INSERT INTO books (id, title, author) VALUES (5, 'Mark Twain', 'The Adventures of Huckleberry Finn');
+
+SELECT setval('books_id_seq', (SELECT MAX(id) FROM books));
--- scripts/db/seed/music.sql
+++ scripts/db/seed/music.sql
@@ -1,2 +1,4 @@
 INSERT INTO musics (id, artist) VALUES (1, 'Maria Carey');
 INSERT INTO musics (id, artist) VALUES (2, 'Agnes Monica');
+
+SELECT setval('musics_id_seq', (SELECT MAX(id) FROM musics));
