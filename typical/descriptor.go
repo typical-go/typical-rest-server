a83acdc3d09a063926a2e169beb4df5abbca6e12
iman tung <iman.tung@gmail.com>
2020-01-23 15:41:47 +0700

    revision: tidy up descriptor

--- typical/descriptor.go
+++ typical/descriptor.go
@@ -12,6 +12,65 @@ import (
 	"github.com/typical-go/typical-rest-server/pkg/typserver"
 )
 
+// Descriptor of Typical REST Server
+// Build-Tool and Application will be generated based on this descriptor
+var Descriptor = typcore.Descriptor{
+
+	// Common project information
+	// Name and Package is mandatory
+	Name:        "Typical REST Server",
+	Description: "Example of typical and scalable RESTful API Server for Go",
+	Version:     "0.8.17",
+	Package:     "github.com/typical-go/typical-rest-server",
+
+	App: typcore.NewApp(application).
+
+		// Dependency is what are provided in dig service-locator
+		// and what to be destroyed after application stop
+		WithDependency(
+			server,   // create and destroy http server
+			redis,    // create and destroy redis connection
+			postgres, // create and destroy postgres db connection
+		).
+
+		// Preparation before start the application
+		WithPrepare(
+			redis,    // Ping to Redis Server
+			postgres, // Ping to Postgres Database
+		),
+
+	Build: typcore.NewBuild().
+
+		// Additional command to be register in Build-Tool
+		WithCommands(
+			docker,
+			readme, // generate readme based on README.tmpl
+			postgres,
+			redis,
+			rails,
+		).
+
+		// Setting to release the project
+		// By default it will create distribution for Darwin and Linux
+		WithRelease(typrls.New().
+			WithPublisher(
+				// Create release and upload file to Github
+				typrls.GithubPublisher("typical-go", "typical-rest-server"),
+			),
+		),
+
+	// Configuration for this project
+	// Both Build-Tool and Application typically using same configuration
+	Configuration: typcore.NewConfiguration().
+		WithConfigure(
+			application,
+			server,
+			redis,
+			postgres,
+		),
+}
+
+// Modules that required for the project
 var (
 	application = app.New()
 	readme      = typreadme.New()
@@ -24,59 +83,4 @@ var (
 		postgres,
 		redis,
 	)
-
-	// Descriptor of Typical REST Server
-	// Build-Tool and Application will be generated based on this descriptor
-	Descriptor = typcore.Descriptor{
-		Name:        "Typical REST Server",
-		Description: "Example of typical and scalable RESTful API Server for Go",
-		Version:     "0.8.17",
-		Package:     "github.com/typical-go/typical-rest-server",
-
-		App: typcore.NewApp(application).
-
-			// Dependency is what are provided in dig service-locator
-			// and what to be destroyed after application stop
-			WithDependency(
-				server,   // create and destroy http server
-				redis,    // create and destroy redis connection
-				postgres, // create and destroy postgres db connection
-			).
-
-			// Preparation before start the application
-			WithPrepare(
-				redis,    // Redis ping
-				postgres, // Postgres Database ping
-			),
-
-		Build: typcore.NewBuild().
-
-			// Additional command to be register in Build-Tool
-			WithCommands(
-				docker,
-				readme, // generate readme based on README.tmpl
-				postgres,
-				redis,
-				rails,
-			).
-
-			// Setting to release the project
-			// By default it will create distribution for Darwin and Linux
-			WithRelease(typrls.New().
-				WithPublisher(
-					// Create release and upload file to Github
-					typrls.GithubPublisher("typical-go", "typical-rest-server"),
-				),
-			),
-
-		// Configuration for this project
-		// Both Build-Tool and Application typically using same configuration
-		Configuration: typcore.NewConfiguration().
-			WithConfigure(
-				application,
-				server,
-				redis,
-				postgres,
-			),
-	}
 )
