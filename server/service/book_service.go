be3617785a5a298ed29042120c1b436984ab95f5
iman tung <iman.tung@gmail.com>
2020-04-19 10:22:44 +0700

    remove the cache store as not necessary

--- server/service/book_service.go
+++ server/service/book_service.go
@@ -2,7 +2,6 @@ package service
 
 import (
 	"context"
-	"fmt"
 
 	"github.com/go-redis/redis"
 	"github.com/typical-go/typical-rest-server/pkg/dbkit"
@@ -30,31 +29,4 @@ type BookServiceImpl struct {
 // NewBookService return new instance of BookService [constructor]
 func NewBookService(impl BookServiceImpl) BookService {
 	return &impl
-}
-
-// FindOne book
-func (r *BookServiceImpl) FindOne(ctx context.Context, id int64) (book *repository.Book, err error) {
-	book = new(repository.Book)
-	var (
-		cacheStore = dbkit.NewCacheStore(r.Redis)
-		key        = fmt.Sprintf("BOOK:FIND_ONE:%d", id)
-	)
-	cacheStore.Retrieve(ctx, key, book, func() (interface{}, error) {
-		book, err = r.BookRepo.FindOne(ctx, id)
-		return book, err
-	})
-	return
-}
-
-// Find books
-func (r *BookServiceImpl) Find(ctx context.Context, opts ...dbkit.FindOption) (list []*repository.Book, err error) {
-	var (
-		cacheStore = dbkit.NewCacheStore(r.Redis)
-		key        = fmt.Sprintf("BOOK:FIND:%+v", opts)
-	)
-	cacheStore.Retrieve(ctx, key, &list, func() (interface{}, error) {
-		list, err := r.BookRepo.Find(ctx, opts...)
-		return list, err
-	})
-	return
 }
