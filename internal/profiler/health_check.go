4f993911ee246c659e5f7300aa8786c7d17a8e5d
iman tung <iman.tung@gmail.com>
2020-08-16 20:04:08 +0700

    add /debug/pprof and /debug/vars for profiling + tidy up

---
+++ internal/profiler/health_check.go
@@ -0,0 +1,36 @@
+package profiler
+
+import (
+	"database/sql"
+
+	"github.com/go-redis/redis"
+	"github.com/labstack/echo/v4"
+	"github.com/typical-go/typical-rest-server/pkg/typrest"
+	"go.uber.org/dig"
+)
+
+type (
+	// HealthCheck for profiler
+	HealthCheck struct {
+		dig.In
+		PG    *sql.DB
+		Redis *redis.Client
+	}
+)
+
+var _ typrest.Router = (*HealthCheck)(nil)
+
+// SetRoute to profiler api
+func (h *HealthCheck) SetRoute(e typrest.Server) error {
+	e.Any("application/health", h.handle)
+	return nil
+}
+
+func (h *HealthCheck) handle(ec echo.Context) error {
+	hc := typrest.HealthCheck{
+		"postgres": h.PG.Ping,
+		"redis":    h.Redis.Ping().Err,
+	}
+	status, message := hc.Result()
+	return ec.JSON(status, message)
+}
--- internal/profiler/router.go
+++ internal/profiler/router.go
@@ -1,34 +1,33 @@
 package profiler
 
 import (
-	"database/sql"
+	"net/http"
 
-	"github.com/go-redis/redis"
 	"github.com/labstack/echo/v4"
 	"github.com/typical-go/typical-rest-server/pkg/typrest"
 	"go.uber.org/dig"
+
+	// enable `/debug/vars`
+	_ "expvar"
+
+	// enable `/debug/pprof` API
+	_ "net/http/pprof"
 )
 
-// Router for profiler
-type Router struct {
-	dig.In
-	PG    *sql.DB
-	Redis *redis.Client
-}
+type (
+	// Router for profiler
+	Router struct {
+		dig.In
+		HealthCheck
+	}
+)
 
 var _ typrest.Router = (*Router)(nil)
 
-// SetRoute to profiler api
-func (h *Router) SetRoute(e typrest.Server) error {
-	e.Any("application/health", h.healthCheck)
+// SetRoute for profiler
+func (r *Router) SetRoute(server typrest.Server) error {
+	r.HealthCheck.SetRoute(server)
+	server.GET("/debug/vars", echo.WrapHandler(http.DefaultServeMux))
+	server.GET("/debug/pprof/*", echo.WrapHandler(http.DefaultServeMux))
 	return nil
-}
-
-func (h *Router) healthCheck(ec echo.Context) error {
-	hc := typrest.HealthCheck{
-		"postgres": h.PG.Ping,
-		"redis":    h.Redis.Ping().Err,
-	}
-	status, message := hc.Result()
-	return ec.JSON(status, message)
 }
