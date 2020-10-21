f8f56ac6536934984e2a4dd4b11de0923aee3288
iman tung <iman.tung@gmail.com>
2020-10-21 18:37:30 +0700

    revision: fix test due to supporting HEAD verb

--- internal/app/domain/mymusic/router_test.go
+++ internal/app/domain/mymusic/router_test.go
@@ -1,19 +1,19 @@
-package mylibrary_test
+package mymusic_test
 
 import (
 	"testing"
 
 	"github.com/labstack/echo/v4"
 	"github.com/stretchr/testify/require"
-	"github.com/typical-go/typical-rest-server/internal/app/domain/mylibrary"
+	"github.com/typical-go/typical-rest-server/internal/app/domain/mymusic"
 	"github.com/typical-go/typical-rest-server/pkg/typrest"
 )
 
 func TestRoute(t *testing.T) {
 	e := echo.New()
-	typrest.SetRoute(e, &mylibrary.Router{})
+	typrest.SetRoute(e, &mymusic.Router{})
 	require.Equal(t, []string{
-		"/mylibrary/books\tGET,POST",
-		"/mylibrary/books/:id\tDELETE,GET,HEAD,PATCH,PUT",
+		"/mymusic/songs\tGET,POST",
+		"/mymusic/songs/:id\tDELETE,GET,HEAD,PATCH,PUT",
 	}, typrest.DumpEcho(e))
 }
