d5dc9290b8bddb9810dcd8c9e05c40c848dec52a
iman tung <iman.tung@gmail.com>
2021-08-05 15:32:13 +0700

    bump to version 0.9.20

--- tools/typical-build/typical-build.go
+++ tools/typical-build/typical-build.go
@@ -14,7 +14,7 @@ import (
 
 var descriptor = typgo.Descriptor{
 	ProjectName:    "typical-rest-server",
-	ProjectVersion: "0.9.19",
+	ProjectVersion: "0.9.20",
 	Environment:    typgo.DotEnv(".env"),
 
 	Tasks: []typgo.Tasker{
