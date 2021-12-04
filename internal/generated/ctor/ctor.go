0027d38efb312d6d372779dc985258184dcecd29
Serghei Iakovlev <egrep@protonmail.ch>
2021-12-04 21:52:00 +0200

    Regenerate ctor to add missed dependencies

--- internal/generated/ctor/ctor.go
+++ internal/generated/ctor/ctor.go
@@ -5,12 +5,14 @@ package ctor
 import (
 	"github.com/typical-go/typical-go/pkg/typapp"
 	a "github.com/typical-go/typical-rest-server/internal/app/infra"
-	b "github.com/typical-go/typical-rest-server/internal/app/service"
+	b "github.com/typical-go/typical-rest-server/internal/app/repo"
+	c "github.com/typical-go/typical-rest-server/internal/app/service"
 )
 
 func init() {
 	typapp.Provide("", a.NewCacheStore)
 	typapp.Provide("", a.NewDatabases)
 	typapp.Provide("", a.NewEcho)
-	typapp.Provide("", b.NewBookSvc)
+	typapp.Provide("", b.NewBookRepo)
+	typapp.Provide("", c.NewBookSvc)
 }
